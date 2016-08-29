e600c695aa0b5fe9466d70917e2ec212031efea1
Christopher Kotfila <chris.kotfila@kitware.com>
2016-08-29 09:51:57 -0400

    Make geoutils installable, add executable hdf2tiff

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='geoutils',
+      version='0.0',
+      description='Simple utilities for working with geospatial data',
+      url='https://github.com/dorukozturk/Geospatial_Utilities',
+      author='Doruk Ozturk',
+      author_email='doruk.ozturk@kitware.com',
+      license='Apache 2.0',
+      packages=['geoutils'],
+      zip_safe=False,
+      entry_points={
+          'console_scripts': [
+              "hdf2tiff=geoutils.hdf2tiff:main"
+          ]
+      }
+)
